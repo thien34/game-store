eed3a83a050c85eafdd6e5530f2953b7426ba6e5
✨khanh🌟 <hoangkhanhvn179@gmail.com>
2025-04-26 04:03:30 +0700

    fix bug

--- src/components/client/index/Banner.tsx
+++ src/components/client/index/Banner.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 
 export default function Banner() {
   return (
-    <section className='py-4'>
-      <div className='content-wrapper-max'>
+    <section className='overflow-hidden py-4'>
+      <div className='content-wrapper-max px-6'>
         <h2 className='mb-6 text-center text-3xl font-bold'>Game Mới Cập Nhật</h2>
-        <div className='relative mx-6 aspect-video h-[550px] w-full overflow-hidden rounded-xl'>
+        <div className='relative aspect-video h-[550px] w-full overflow-hidden rounded-xl'>
           <Image
             src='/images/banner.png'
             alt='Game Mới Cập Nhật'
--- src/components/client/index/GameList.tsx
+++ src/components/client/index/GameList.tsx
@@ -5,8 +5,8 @@ export default async function GameList() {
   const { data: games } = await gameService.getAllGames()
 
   return (
-    <div className='mx-6'>
-      <div className='grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
+    <div className='content-wrapper-max'>
+      <div className='mx-6 grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
         {games.map(game => (
           <GameCard
             key={game.id}
